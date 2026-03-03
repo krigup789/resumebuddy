613b18ee2b3f1dbc92ddc208731bbb609ce55cb9
Krish Gupta <krigup789@gmail.com>
2026-03-03 18:10:18 +0530

    Initial project files

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-blue-800 text-center bg-linear-to-r from-[#2009f1] to-[#FDFEFF]">
+      <p>
+        <span className="px-3 py-1 rounded-lg text-white bg-blue-600 mr-2">
+          New
+        </span>
+        AI Website Builder
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
